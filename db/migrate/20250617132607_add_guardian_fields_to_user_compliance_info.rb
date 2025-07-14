c0362eb3804d7234d1c231c85e4d38bd82b18b44
Paul Nizet <polonizet@gmail.com>
2025-07-14 17:07:04 +0200

    Revert: Remove Guardian fields migration from UserComplianceInfo

--- db/migrate/20250617132607_add_guardian_fields_to_user_compliance_info.rb
+++
@@ -1,8 +0,0 @@
-class AddGuardianFieldsToUserComplianceInfo < ActiveRecord::Migration[7.0]
-  def change
-    add_column :user_compliance_info, :guardian_dob_day, :integer
-    add_column :user_compliance_info, :guardian_dob_month, :integer
-    add_column :user_compliance_info, :guardian_dob_year, :integer
-    add_column :user_compliance_info, :guardian_individual_tax_id, :text
-  end
-end
